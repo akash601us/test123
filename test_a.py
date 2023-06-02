e888fc817bbb4488a275137a4f4b2775c15e4412
akash601us <118945304+akash601us@users.noreply.github.com>
2023-06-02 16:46:21 -0400

    changes

--- test_a.py
+++ test_a.py
@@ -1,2 +1,17 @@
 print("Hello World")
-print("Hello %s , simulation can be done using %s ." % ("World", "Python"))
+print("Hello %s , simulation can be done using %s." % ("World", "Python"))
+a=3
+b=6
+
+print(a+6)
+print((a+b)/2)
+print(int((a+b)/2))
+a = 11
+print(hex(a))
+b = 3.2
+print(oct(int(b)))
+d = 0X1A
+print(d)
+print(b+d)
+
+
