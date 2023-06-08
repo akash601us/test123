1d9e7e4c020bfc21b3e27b64131beea8efbecc89
akash601us <118945304+akash601us@users.noreply.github.com>
2023-06-08 17:30:34 -0400

    changes

---
+++ classes.py
@@ -0,0 +1,26 @@
+# class Car:
+#     def suv(self, abc ):
+#         print(abc)
+#     def hatchback(self, xyz):
+#
+# # xyz = Car()
+# # xyz.suv("crv")
+# # xyz.hatchback("mustang")
+#
+# honda = ['accord', 'crv', 'hrv', 'civic']
+# suv = ['crv', 'hrv']
+# hatchback = ['accord', 'civic']
+#
+# if 'accord' in honda:
+#     print("Accord is a Honda model")
+# else:
+#     print("Not a Honda model")
+
+
+
+class Car:
+    def carModel(self, ):
+        print("this is suv")
+
+c = Car()
+c.suv()
--- test_a.py
+++ test_a.py
@@ -25,11 +25,18 @@ firstName= "Akash"
 print(firstName)
 fullName= "Akash R Patel"
 print(fullName)
-score1 = input("Enter the score 1: 49")
-score2 = input("Enter the score 2: 99")
+score1 = input("Enter the score 1: ")
+score2 = input("Enter the score 2: ")
 totalString = score1 + score2
 messageString = "Total score is %s"
 print(messageString % totalString)
 totalInt = int(score1) + int(score2)
 messageString = "Total score is %s"
 print(messageString % totalInt)
+score1 = int(input("Enter the score 1: "))
+score2 = int(input("Enter the score 2: "))
+total = score1 + score2
+messageString = "score1(%s) + score2[%s] = %s"
+print(messageString % (score1, score2,total))
+a = [24, "as it is", "abc", 2+2j]
+print (a[0])
