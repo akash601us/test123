237270ef5c01a481686efb9e9841b4bc176dc38a
akash601us <118945304+akash601us@users.noreply.github.com>
2023-06-09 16:35:50 -0400

    changes

--- classes.py
+++ classes.py
@@ -1,12 +1,31 @@
-# class Car:
-#     def suv(self, abc ):
+man = {'Honda': ['accord', 'crv', 'hrv', 'civic'],
+       'Maruti': ['Breeja', 'Alto']}
+suv = ['crv', 'hrv']
+hatchback = ['accord', 'civic']
+class Car():
+
+    def suv(self,abc): #abc='crv'
+        if abc in suv:
+            print("yes its a suv")
+        else:
+            print("Not its not")
+        for i in man: #i =honda and Maruti
+            print(i)
+            if abc in man[i]:
+                print("Manufacture is ",i)
+
+
+        def hatchback(self, xyz):
+            return True
+
+
+xyz = Car()
+xyz.suv("Alto")
+# self, abc ):
 #         print(abc)
-#     def hatchback(self, xyz):
-#
-# # xyz = Car()
-# # xyz.suv("crv")
-# # xyz.hatchback("mustang")
-#
+# xyz.hatchback("mustang")
+
+
 # honda = ['accord', 'crv', 'hrv', 'civic']
 # suv = ['crv', 'hrv']
 # hatchback = ['accord', 'civic']
@@ -15,12 +34,13 @@
 #     print("Accord is a Honda model")
 # else:
 #     print("Not a Honda model")
-
-
-
-class Car:
-    def carModel(self, ):
-        print("this is suv")
-
-c = Car()
-c.suv()
+#
+#
+#
+# class Car:
+#     def car_model(self, X ):
+#         if x=
+#         print("this is suv")
+#
+# c = Car()
+# c.suv()
