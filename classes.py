31cf400cb24a8b8be5138473cba733eed14af413
akash601us <118945304+akash601us@users.noreply.github.com>
2023-06-12 17:27:22 -0400

    Update classes.py

--- classes.py
+++ classes.py
@@ -22,23 +22,3 @@ class Car():
 xyz = Car()
 xyz.suv("Alto")
 
-
-Subject = {'Science': ['maths', 'physics', 'chemistry', 'biology'],
-           'commerce': ['economics', 'business studies', 'financial management', 'policy'],
-           'Arts': ['history', 'geography', 'political science']}
-
-engineering = ['maths', 'physics']
-medical = ['biology', 'chemistry']
-accounting = ['business studies', 'policy']
-journalism = ['history', 'political science']
-
-
-class Stream():
-
-    def engineering(self, abc):
-        if abc in engineering:
-            print("yes you are eligible for engineering")
-        else:
-            print("No You are not eligible for engineering")
-        for i in subject:
-            print
